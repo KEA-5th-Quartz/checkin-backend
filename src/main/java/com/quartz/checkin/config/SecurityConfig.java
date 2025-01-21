eb767800c1bb00ddf4712623f235e78411fc340e
chanhoim <cksgh201@naver.com>
2025-01-21 16:26:16 +0900

    QUARTZ-49 [Feat] 시큐리티 Config 클래스 작성
    개발 시 API 테스트에 문제가 없도록 모든 요청을 허용 처리

---
+++ src/main/java/com/quartz/checkin/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.quartz.checkin.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(
+                        FrameOptionsConfig::disable
+                ))
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll());
+
+        return http.build();
+    }
+
+}

992fb5f0c3d034d67a5893159e69f5cc33408991
chanhoim <cksgh201@naver.com>
2025-01-21 16:14:42 +0900

    QUARTZ-49 [Feat] CustomException 작성 및 CustomExceptionHandler 구현

---
+++ src/main/java/com/quartz/checkin/exception/custom/CustomException.java
@@ -0,0 +1,16 @@
+package com.quartz.checkin.exception.custom;
+
+import com.quartz.checkin.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+}

fd8c687e08c31c055f40a7893ce25b9507cb582c
chogh824 <chogh824@naver.com>
2025-01-23 20:13:17 +0900

    refactor: 예외 처리 부분 수정

--- src/main/java/com/quartz/checkin/common/exception/ApiCode.java
+++ src/main/java/com/quartz/checkin/common/exception/ApiCode.java
@@ -1,14 +1,12 @@
 package com.quartz.checkin.common.exception;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
-@RequiredArgsConstructor
 public enum ApiCode {
+
     // 공통 예외
-    OK(HttpStatus.OK, "COMMON_2000", "OK"),
     INVALID_DATA(HttpStatus.BAD_REQUEST, "COMMON_4000", "필수로 요구되는 데이터가 비어있거나 규칙에 맞지 않습니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON_4010", "유효하지 않거나 만료된 토큰입니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON_4030", "접근 권한이 없습니다."),
@@ -20,46 +18,71 @@ public enum ApiCode {
     DB_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_5001", "데이터베이스 오류가 발생했습니다."),
     OBJECT_STORAGE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_5002", "오브젝트 스토리지 오류가 발생했습니다."),
 
-    // 회원 서비스
-    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "MEMBER_4000", "기존 비밀번호와 일치하지 않습니다."),
-    INVALID_EMAIL_FORMAT(HttpStatus.BAD_REQUEST, "MEMBER_4001", "이메일 형식이 올바르지 않습니다."),
-    INVALID_USERNAME(HttpStatus.BAD_REQUEST, "MEMBER_4002", "아이디 형식이 올바르지 않습니다."),
+    // 회원 서비스 예외
     INVALID_PASSWORD_FORMAT(HttpStatus.BAD_REQUEST, "MEMBER_4003", "비밀번호 형식이 올바르지 않습니다."),
-    PASSWORD_CONFIRM_MISMATCH(HttpStatus.BAD_REQUEST, "MEMBER_4004", "비밀번호 확인이 일치하지 않습니다."),
-    MISSING_USERNAME(HttpStatus.BAD_REQUEST, "MEMBER_4005", "아이디 값이 누락되었습니다."),
-    INVALID_ROLE(HttpStatus.BAD_REQUEST, "MEMBER_4006", "잘못된 역할 값이 제공되었습니다."),
-    INVALID_PAGE_NUMBER(HttpStatus.BAD_REQUEST, "MEMBER_4007", "유효하지 않은 페이지 번호입니다."),
-    INVALID_PAGE_SIZE(HttpStatus.BAD_REQUEST, "MEMBER_4008", "유효하지 않은 페이지 크기입니다."),
-    LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "MEMBER_4010", "아이디 또는 비밀번호가 올바르지 않습니다."),
-    PASSWORD_INCORRECT(HttpStatus.UNAUTHORIZED, "MEMBER_4011", "비밀번호가 올바르지 않습니다. 다시 시도해주세요."),
-    LOGIN_REQUIRED(HttpStatus.UNAUTHORIZED, "MEMBER_4012", "로그인이 필요한 요청입니다."),
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_4040", "존재하지 않는 회원입니다."),
-    EMAIL_ALREADY_USED(HttpStatus.CONFLICT, "MEMBER_4091", "이미 사용 중인 이메일 주소입니다."),
+    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "MEMBER_4004", "비밀번호가 일치하지 않습니다."),
+    USERNAME_MISSING(HttpStatus.BAD_REQUEST, "MEMBER_4005", "아이디 값이 누락되었습니다."),
+    INVALID_PAGE_NUMBER(HttpStatus.BAD_REQUEST, "MEMBER_4007", "페이지 번호가 유효하지 않습니다."),
+    INVALID_PAGE_SIZE(HttpStatus.BAD_REQUEST, "MEMBER_4008", "페이지 크기가 유효하지 않습니다."),
+    MEMBER_NOT_FOUND(HttpStatus.UNAUTHORIZED, "MEMBER_4013", "일치하는 회원 정보가 없습니다. 아이디 혹은 비밀번호를 다시 확인해주세요."),
+    UNSUPPORTED_FILE_TYPE(HttpStatus.UNAUTHORIZED, "MEMBER_4015", "지원하지 않는 파일 형식입니다."),
+    MEMBER_DOES_NOT_EXIST(HttpStatus.NOT_FOUND, "MEMBER_4040", "존재하지 않는 회원입니다."),
+    NOT_REGISTERED(HttpStatus.NOT_FOUND, "MEMBER_4041", "가입하지 않은 아이디입니다."),
+    DUPLICATE_USERNAME(HttpStatus.CONFLICT, "MEMBER_4090", "이미 사용 중인 아이디입니다."),
+    DUPLICATE_EMAIL(HttpStatus.CONFLICT, "MEMBER_4091", "이미 사용 중인 이메일 주소입니다."),
+    ADMIN_PERMISSION_REQUIRED(HttpStatus.CONFLICT, "MEMBER_4093", "관리자 권한이 없는 회원에게 최고 관리자 권한을 줄 수 없습니다."),
 
-    // 템플릿 서비스
+    // 템플릿 서비스 예외
     TEMPLATE_NOT_FOUND(HttpStatus.NOT_FOUND, "TEMPLATE_4040", "존재하지 않는 템플릿입니다."),
-    DUPLICATE_TEMPLATE(HttpStatus.CONFLICT, "TEMPLATE_4090", "동일한 제목의 템플릿이 이미 존재합니다."),
+    DUPLICATE_TEMPLATE_TITLE(HttpStatus.CONFLICT, "TEMPLATE_4090", "동일한 제목을 가진 템플릿이 이미 존재합니다."),
 
-    // 티켓 서비스
-    INVALID_TICKET_TITLE(HttpStatus.BAD_REQUEST, "TICKET_4000", "유효하지 않은 티켓 제목입니다."),
-    INVALID_TICKET_DESCRIPTION(HttpStatus.BAD_REQUEST, "TICKET_4001", "유효하지 않은 티켓 설명입니다."),
-    INVALID_TICKET_STATUS(HttpStatus.BAD_REQUEST, "TICKET_4003", "유효하지 않은 티켓 상태 값입니다."),
+    // 티켓 서비스 예외
+    INVALID_TICKET_TITLE(HttpStatus.BAD_REQUEST, "TICKET_4000", "제목이 유효하지 않습니다."),
+    INVALID_TICKET_DESCRIPTION(HttpStatus.BAD_REQUEST, "TICKET_4001", "설명이 유효하지 않습니다."),
+    INVALID_TICKET_DUE_DATE(HttpStatus.BAD_REQUEST, "TICKET_4002", "마감 기한이 유효하지 않습니다."),
+    INVALID_TICKET_STATUS(HttpStatus.BAD_REQUEST, "TICKET_4003", "티켓 상태 값이 유효하지 않습니다."),
+    INVALID_TICKET_PRIORITY(HttpStatus.BAD_REQUEST, "TICKET_4004", "중요도 값이 유효하지 않습니다."),
+    INVALID_TICKET_CATEGORY(HttpStatus.BAD_REQUEST, "TICKET_4005", "카테고리 값이 유효하지 않습니다."),
+    INVALID_TICKET_MANAGER(HttpStatus.BAD_REQUEST, "TICKET_4006", "담당자 정보가 유효하지 않습니다."),
+    INVALID_TICKET_PAGE_NUMBER(HttpStatus.BAD_REQUEST, "TICKET_4007", "페이지 번호가 유효하지 않습니다."),
+    INVALID_TICKET_PAGE_SIZE(HttpStatus.BAD_REQUEST, "TICKET_4008", "페이지 크기가 유효하지 않습니다."),
+    CANNOT_CHANGE_COMPLETED_TICKET(HttpStatus.BAD_REQUEST, "TICKET_4009", "이미 완료된 티켓은 상태를 변경할 수 없습니다."),
+    UNSUPPORTED_TICKET_FILE_TYPE(HttpStatus.UNAUTHORIZED, "TICKET_4015", "지원하지 않는 파일 형식입니다."),
+    TICKET_FILE_TOO_LARGE(HttpStatus.UNAUTHORIZED, "TICKET_4016", "파일 크기가 초과되었습니다."),
+    TICKET_FILE_NOT_FOUND(HttpStatus.UNAUTHORIZED, "TICKET_4017", "파일이 존재하지 않습니다."),
     TICKET_NOT_FOUND(HttpStatus.NOT_FOUND, "TICKET_4040", "존재하지 않는 티켓입니다."),
+    TICKET_KEYWORD_NOT_FOUND(HttpStatus.NOT_FOUND, "TICKET_4041", "해당 키워드를 가진 티켓이 존재하지 않습니다."),
 
-    // 댓글 서비스
+    // 댓글 서비스 예외
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMENT_4040", "존재하지 않는 댓글입니다."),
 
-    // 카테고리 서비스
-    MISSING_CATEGORY_NAME(HttpStatus.BAD_REQUEST, "CATEGORY_4000", "카테고리 이름이 누락되었습니다."),
-    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "CATEGORY_4040", "존재하지 않는 카테고리입니다."),
-    DUPLICATE_CATEGORY(HttpStatus.CONFLICT, "CATEGORY_4090", "동일한 이름의 카테고리가 이미 존재합니다."),
+    // 카테고리 서비스 예외
+    CATEGORY_NAME_MISSING(HttpStatus.BAD_REQUEST, "CATEGORY_4000", "카테고리 이름이 누락되었습니다."),
+    INVALID_CATEGORY_NAME_FORMAT(HttpStatus.BAD_REQUEST, "CATEGORY_4001", "카테고리 이름 형식이 올바르지 않습니다."),
+    CATEGORY_NOT_FOUND_FIRST(HttpStatus.NOT_FOUND, "CATEGORY_4040", "존재하지 않는 1차 카테고리입니다."),
+    CATEGORY_NOT_FOUND_SECOND(HttpStatus.NOT_FOUND, "CATEGORY_4041", "존재하지 않는 2차 카테고리입니다."),
+    DUPLICATE_CATEGORY_FIRST(HttpStatus.CONFLICT, "CATEGORY_4090", "동일한 이름의 1차 카테고리가 이미 존재합니다."),
+    DUPLICATE_CATEGORY_SECOND(HttpStatus.CONFLICT, "CATEGORY_4091", "동일한 이름의 2차 카테고리가 이미 존재합니다."),
+    CATEGORY_HAS_SUBCATEGORIES(HttpStatus.CONFLICT, "CATEGORY_4092", "1차 카테고리에 속한 2차 카테고리가 존재하여 삭제할 수 없습니다."),
 
-    // 통계 서비스
-    MISSING_DATE_RANGE(HttpStatus.BAD_REQUEST, "STATS_4000", "시작 날짜 또는 종료 날짜가 누락되었습니다."),
-    INVALID_DATE_FORMAT(HttpStatus.BAD_REQUEST, "STATS_4001", "날짜 형식이 올바르지 않습니다."),
-    STAT_NOT_FOUND(HttpStatus.NOT_FOUND, "STATS_4041", "존재하지 않는 통계 데이터입니다.");
+    // 통계 서비스 예외
+    STATS_MISSING_DATE(HttpStatus.BAD_REQUEST, "STATS_4000", "시작 날짜 또는 종료 날짜가 누락되었습니다."),
+    INVALID_STATS_DATE_FORMAT(HttpStatus.BAD_REQUEST, "STATS_4001", "날짜 형식이 올바르지 않습니다."),
+    INVALID_STATS_DATE_RANGE(HttpStatus.BAD_REQUEST, "STATS_4002", "시작 날짜가 종료 날짜보다 큽니다."),
+    STATS_MANAGER_NOT_FOUND(HttpStatus.NOT_FOUND, "STATS_4041", "존재하지 않는 담당자입니다.");
 
-    private final HttpStatus httpStatus;
+    private final HttpStatus status;
     private final String code;
     private final String message;
+
+    ApiCode(HttpStatus httpStatus, String code, String message) {
+        this.status= httpStatus;
+        this.code = code;
+        this.message = message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return this.status;
+    }
+
 }
--- src/main/java/com/quartz/checkin/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/quartz/checkin/common/exception/GlobalExceptionHandler.java
@@ -2,12 +2,15 @@ package com.quartz.checkin.common.exception;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Path;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,6 +18,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import software.amazon.awssdk.core.exception.SdkClientException;
+import software.amazon.awssdk.core.exception.SdkException;
 
 
 import java.util.*;
@@ -24,80 +28,102 @@ import java.util.*;
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     // 커스텀 예외 처리
-    @ExceptionHandler(ApiException.class)
-    public ResponseEntity<Object> handleCustomException(ApiException ex) {
-        log.error("ApiException 발생: {}", ex.getMessage(), ex);
-        return buildErrorResponse(ex.getApiCode());
+    @ExceptionHandler
+    public ResponseEntity<Object> HandleCustomException(ApiException ex) {
+        return handleExceptionInternal(ex.getApiCode());
     }
 
     // @Valid 검증 예외 처리
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 
-        log.warn("ValidationException 발생: {}", ex.getMessage());
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
 
         List<Map<String, String>> errors = new ArrayList<>();
-        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+        for (FieldError fieldError : fieldErrors) {
+
             Map<String, String> error = new HashMap<>();
             error.put("field", fieldError.getField());
             error.put("message", fieldError.getDefaultMessage());
+
             errors.add(error);
         }
 
-        return buildErrorResponse(ApiCode.INVALID_DATA, errors);
+        return handleExceptionInternal(ApiCode.INVALID_DATA, errors);
     }
 
     // @Validated 검증 예외 처리
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex) {
-        log.warn("ConstraintViolationException 발생: {}", ex.getMessage());
+    @ExceptionHandler
+    protected ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex) {
+
+        Set<ConstraintViolation<?>> fieldErrors = ex.getConstraintViolations();
 
         List<Map<String, String>> errors = new ArrayList<>();
-        for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+        for (ConstraintViolation<?> fieldError : fieldErrors) {
             Map<String, String> error = new HashMap<>();
-            error.put("field", violation.getPropertyPath().toString());
-            error.put("message", violation.getMessage());
+
+            String field = null;
+            for (Path.Node node : fieldError.getPropertyPath()) {
+                field = node.getName();
+            }
+            error.put("field", field);
+            error.put("message", fieldError.getMessage());
+
             errors.add(error);
         }
 
-        return buildErrorResponse(ApiCode.INVALID_DATA, errors);
+        return handleExceptionInternal(ApiCode.INVALID_DATA, errors);
     }
 
     // API 인자 불일치 예외 처리
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
-        log.warn("MethodArgumentTypeMismatchException 발생: {}", ex.getMessage());
-        return buildErrorResponse(ApiCode.METHOD_NOT_ALLOWED);
+    public ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException e) {
+        return handleExceptionInternal(ApiCode.METHOD_NOT_ALLOWED);
     }
 
     // DB 유효성 예외 처리
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
-        log.error("DataIntegrityViolationException 발생: {}", ex.getMessage(), ex);
-        return buildErrorResponse(ApiCode.DB_ERROR);
+    @ExceptionHandler
+    public ResponseEntity<Object> HandleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        return handleExceptionInternal(ApiCode.DB_ERROR);
     }
 
     // S3 API 예외 처리
-    @ExceptionHandler(SdkClientException.class)
-    public ResponseEntity<Object> handleAwsServiceException(SdkClientException ex) {
-        log.error("AWS SDK Exception 발생: {}", ex.getMessage(), ex);
-        return buildErrorResponse(ApiCode.OBJECT_STORAGE_ERROR, ex.getMessage());
+    @ExceptionHandler
+    public ResponseEntity<Object> HandleAwsServiceException(SdkException e) {
+        return handleExceptionInternal(ApiCode.OBJECT_STORAGE_ERROR, e.getMessage());
     }
 
     // 그 외 모든 예외 처리
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleException(Exception ex) {
-        log.error("Unexpected Exception 발생: {}", ex.getMessage(), ex);
-        return buildErrorResponse(ApiCode.INTERNAL_SERVER_ERROR, ex.getMessage());
+    @ExceptionHandler
+    public ResponseEntity<Object> handleException(Exception e) {
+        log.error(e.getMessage(), e);
+
+        return handleExceptionInternal(ApiCode.INTERNAL_SERVER_ERROR, e.getMessage());
     }
 
-    // 공통 응답 빌더
-    private ResponseEntity<Object> buildErrorResponse(ApiCode apiCode) {
+    private ResponseEntity<Object> handleExceptionInternal (ApiCode apiCode) {
         return ResponseEntity.status(apiCode.getHttpStatus()).body(ApiResponse.onFailure(apiCode));
     }
 
-    private ResponseEntity<Object> buildErrorResponse(ApiCode apiCode, Object errors) {
+    private ResponseEntity<Object> handleExceptionInternal (ApiCode apiCode, Object errors) {
         return ResponseEntity.status(apiCode.getHttpStatus()).body(ApiResponse.onFailure(apiCode, errors));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        log.error(ex.getMessage(), ex);
+
+        String message;
+        if (ex instanceof ErrorResponse errorResponse)
+            if (body == null)
+                message = errorResponse.updateAndGetBody(this.getMessageSource(), LocaleContextHolder.getLocale()).getDetail();
+            else
+                message = ((ErrorResponse) body).getDetailMessageCode();
+        else
+            message = ex.getMessage();
+
+        body = ApiResponse.onFailure(statusCode.value(), message);
+
+        return ResponseEntity.status(statusCode.value()).body(body);
     }
 }

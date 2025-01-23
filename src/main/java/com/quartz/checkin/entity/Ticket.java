9b9bc0145605c03bd95dc801ca8b2cd066874e52
chogh824 <chogh824@naver.com>
2025-01-23 18:00:21 +0900

    refactor: 엔티티 수정

--- src/main/java/com/quartz/checkin/entity/Ticket.java
+++ src/main/java/com/quartz/checkin/entity/Ticket.java
@@ -1,21 +1,16 @@
 package com.quartz.checkin.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Ticket extends BaseEntity {
 
     @Id
@@ -46,7 +41,7 @@ public class Ticket extends BaseEntity {
     private String content;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column
     private Priority priority;
 
     @Enumerated(EnumType.STRING)
@@ -59,4 +54,15 @@ public class Ticket extends BaseEntity {
     private LocalDateTime closed_at;
 
     private LocalDateTime deleted_at;
+
+    @Builder
+    public Ticket(Member user, Category firstCategory, Category secondCategory, String title, String content, Status status, LocalDate dueDate) {
+        this.user = user;
+        this.firstCategory = firstCategory;
+        this.secondCategory = secondCategory;
+        this.title = title;
+        this.content = content;
+        this.status = status;
+        this.dueDate = dueDate;
+    }
 }

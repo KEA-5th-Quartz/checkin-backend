03c3d1930939c4ac37582543b1580c48f9917a6b
chanhoim <cksgh201@naver.com>
2025-01-24 20:57:01 +0900

    QUARTZ-51 [Feat] role 검증 어노테이션 작성
    사용자 생성 API의 요청으로 들어오는 enum값을 어노테이션을 통해 스프링 validation을 받을 수 있게 처리합니다.

---
+++ src/main/java/com/quartz/checkin/utils/validator/RoleValidator.java
@@ -0,0 +1,17 @@
+package com.quartz.checkin.utils.validator;
+
+import com.quartz.checkin.entity.Role;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class RoleValidator implements ConstraintValidator<ValidRole, String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return false;
+        }
+
+        return Role.isRole(value);
+    }
+}
---
+++ src/main/java/com/quartz/checkin/utils/validator/ValidRole.java
@@ -0,0 +1,16 @@
+package com.quartz.checkin.utils.validator;
+
+import jakarta.validation.Constraint;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = RoleValidator.class)
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
+public @interface ValidRole {
+    String message() default "유효하지 않은 권한입니다.";
+    Class<?>[] groups() default {};
+    Class<? extends jakarta.validation.Payload>[] payload() default {};
+}

8a187d194a934a89e27c7edc6940c1fc117b2a62
chogh824 <chogh824@naver.com>
2025-01-25 20:01:15 +0900

    QUARTZ-60 [Fix] 빌드 오류 수정

--- src/main/java/com/quartz/checkin/entity/TicketLog.java
+++ src/main/java/com/quartz/checkin/entity/TicketLog.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter

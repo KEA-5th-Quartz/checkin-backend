0e8e381cd77ee8b90af5c6d51d7666c76c994ae8
chogh824 <chogh824@naver.com>
2025-01-25 15:41:01 +0900

    QUARTZ-60 [Chore] 티켓 생성 response 수정

--- src/main/java/com/quartz/checkin/dto/response/TicketCreateResponse.java
+++ src/main/java/com/quartz/checkin/dto/response/TicketCreateResponse.java
@@ -1,13 +1,14 @@
 package com.quartz.checkin.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@AllArgsConstructor
 public class TicketCreateResponse {
-    private Long ticketId;
-    private LocalDateTime createdAt;
+    private final Long ticketId;
+
+    public TicketCreateResponse(Long ticketId) {
+        this.ticketId = ticketId;
+    }
 }

ce089d7990c200da100b53f37c4f5012746876c9
chogh824 <chogh824@naver.com>
2025-01-26 19:31:24 +0900

    QUARTZ-61 [Refactor] 티켓 생성 response 클래스 레코드로 수정

--- src/main/java/com/quartz/checkin/dto/response/TicketCreateResponse.java
+++ src/main/java/com/quartz/checkin/dto/response/TicketCreateResponse.java
@@ -2,13 +2,7 @@ package com.quartz.checkin.dto.response;
 
 import lombok.Getter;
 
-import java.time.LocalDateTime;
 
 @Getter
-public class TicketCreateResponse {
-    private final Long ticketId;
-
-    public TicketCreateResponse(Long ticketId) {
-        this.ticketId = ticketId;
-    }
+public record TicketCreateResponse(Long ticketId) {
 }

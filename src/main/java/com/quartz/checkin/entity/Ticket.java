0f5f4a24c15a886374b87f9af373ac0de6dc357d
chogh824 <chogh824@naver.com>
2025-01-28 16:10:45 +0900

    QUARTZ-61 [Refactor] 와일드카드 import 문 지양

--- src/main/java/com/quartz/checkin/entity/Ticket.java
+++ src/main/java/com/quartz/checkin/entity/Ticket.java
@@ -1,6 +1,17 @@
 package com.quartz.checkin.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;

866426b8ee59bad3bb7a656e2941d4a1bd885d1a
chogh824 <chogh824@naver.com>
2025-02-05 17:39:15 +0900

    QUARTZ-91 [Refactor] 티켓 댓글, 티켓 제목, 내용 글자 제한

--- src/main/java/com/quartz/checkin/dto/ticket/request/TicketCreateRequest.java
+++ src/main/java/com/quartz/checkin/dto/ticket/request/TicketCreateRequest.java
@@ -2,9 +2,10 @@ package com.quartz.checkin.dto.ticket.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,9 +15,11 @@ import lombok.NoArgsConstructor;
 public class TicketCreateRequest {
 
     @NotBlank
+    @Size(max = 25, message = "제목은 최대 25자까지 입력할 수 있습니다.")
     private String title;
 
     @NotBlank
+    @Size(max = 256, message = "내용은 최대 256자까지 입력할 수 있습니다.")
     private String content;
 
     @NotBlank
--- src/main/java/com/quartz/checkin/dto/ticket/request/TicketUpdateRequest.java
+++ src/main/java/com/quartz/checkin/dto/ticket/request/TicketUpdateRequest.java
@@ -1,12 +1,15 @@
 package com.quartz.checkin.dto.ticket.request;
 
+import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.Getter;
 
 @Getter
 public class TicketUpdateRequest {
+    @Size(max = 25, message = "제목은 최대 512자까지 입력할 수 있습니다.")
     private String title;
+    @Size(max = 256, message = "내용은 최대 256자까지 입력할 수 있습니다.")
     private String content;
     private String firstCategory;
     private String secondCategory;

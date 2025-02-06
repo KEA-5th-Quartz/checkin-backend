2d1c3c3cc8b367e74b9444cf6c535630efe90d19
chogh824 <chogh824@naver.com>
2025-02-06 10:42:18 +0900

    QUARTZ-95 [Feat] Alias에 대한 에러 코드 작성

--- src/main/java/com/quartz/checkin/common/exception/ErrorCode.java
+++ src/main/java/com/quartz/checkin/common/exception/ErrorCode.java
@@ -62,6 +62,7 @@ public enum ErrorCode {
     // 카테고리 서비스 예외
     CATEGORY_NAME_MISSING(HttpStatus.BAD_REQUEST, "CATEGORY_4000", "카테고리 이름이 누락되었습니다."),
     INVALID_CATEGORY_NAME_FORMAT(HttpStatus.BAD_REQUEST, "CATEGORY_4001", "카테고리 이름 형식이 올바르지 않습니다."),
+    INVALID_ALIAS_FORMAT(HttpStatus.BAD_REQUEST,"CATEGORY_4002", "약어 형식이 올바르지 않습니다."),
     CATEGORY_NOT_FOUND_FIRST(HttpStatus.NOT_FOUND, "CATEGORY_4040", "존재하지 않는 1차 카테고리입니다."),
     CATEGORY_NOT_FOUND_SECOND(HttpStatus.NOT_FOUND, "CATEGORY_4041", "존재하지 않는 2차 카테고리입니다."),
     DUPLICATE_CATEGORY_FIRST(HttpStatus.CONFLICT, "CATEGORY_4090", "동일한 이름의 1차 카테고리가 이미 존재합니다."),

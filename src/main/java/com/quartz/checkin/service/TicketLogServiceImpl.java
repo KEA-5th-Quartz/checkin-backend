596c3831ec8b2a9966e0e65cd9ff71ac7f650046
chan9e <pmkrhgh@gmail.com>
2025-02-06 16:53:02 +0900

    QUARTZ-90 [Merge] develop 브랜치와 병합

--- src/main/java/com/quartz/checkin/service/TicketLogServiceImpl.java
+++ src/main/java/com/quartz/checkin/service/TicketLogServiceImpl.java
@@ -22,6 +22,7 @@ import com.quartz.checkin.repository.TicketLogRepository;
 import com.quartz.checkin.repository.TicketRepository;
 import jakarta.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import java.util.Set;

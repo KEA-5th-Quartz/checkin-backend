88195bc526ed949fc39da946498488b566c1ba80
chogh824 <chogh824@naver.com>
2025-02-07 18:22:04 +0900

    QUARTZ-120 [Refactor] 티켓 번호 형식 QueryDsl로 수정

--- src/main/java/com/quartz/checkin/repository/TicketRepository.java
+++ src/main/java/com/quartz/checkin/repository/TicketRepository.java
@@ -2,18 +2,6 @@ package com.quartz.checkin.repository;
 
 import com.quartz.checkin.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
-    // Todo: QueryDSL로 구현
-
-    @Query("""
-    SELECT t.customId 
-    FROM Ticket t 
-    WHERE t.customId LIKE CONCAT(:prefix, '%') 
-    ORDER BY CAST(SUBSTRING(t.customId, LENGTH(:prefix) + 1, 3) AS int) DESC 
-    LIMIT 1
-""")
-    String findLastTicketId(@Param("prefix") String prefix);
 }

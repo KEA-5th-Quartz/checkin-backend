6d59a56e90af60863b40659c2a27ba99fbedd91b
chanhoim <cksgh201@naver.com>
2025-02-07 12:04:18 +0900

    QUARTZ-96 [Feat] accessToken 블랙리스트 캐시 구현

--- src/main/java/com/quartz/checkin/config/CacheConfig.java
+++ src/main/java/com/quartz/checkin/config/CacheConfig.java
@@ -2,6 +2,7 @@ package com.quartz.checkin.config;
 
 import com.quartz.checkin.common.cache.LoginBlockCache;
 import com.quartz.checkin.common.cache.LoginFailureCache;
+import com.quartz.checkin.common.cache.TokenBlacklistCache;
 import java.util.List;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -15,13 +16,15 @@ public class CacheConfig {
 
     public static final String LOGIN_BLOCK_CACHE = "loginBlock";
     public static final String LOGIN_FAILURE_CACHE = "loginFailure";
+    public static final String TOKEN_BLACKLIST_CACHE = "tokenBlacklist";
 
     @Bean
     public CacheManager cacheManager() {
         SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
         simpleCacheManager.setCaches(List.of(
                 new LoginFailureCache(LOGIN_FAILURE_CACHE),
-                new LoginBlockCache(LOGIN_BLOCK_CACHE)
+                new LoginBlockCache(LOGIN_BLOCK_CACHE),
+                new TokenBlacklistCache(TOKEN_BLACKLIST_CACHE)
         ));
         simpleCacheManager.afterPropertiesSet();
         return simpleCacheManager;

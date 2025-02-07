140d0cf57ed74137e24f9c184926b5b9230f925a
chanhoim <cksgh201@naver.com>
2025-02-07 22:15:41 +0900

    QUARTZ-119 [Fix] EnableScheduling 어노테이션 중복 제거

--- src/main/java/com/quartz/checkin/config/CacheConfig.java
+++ src/main/java/com/quartz/checkin/config/CacheConfig.java
@@ -10,12 +10,10 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 @EnableCaching
-@EnableScheduling
 public class CacheConfig {
 
     public static final String LOGIN_BLOCK_CACHE = "loginBlock";

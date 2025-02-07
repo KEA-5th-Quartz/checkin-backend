d7b2aeed5e29c7ef7a3ab4bf5f30d78d544ec375
chanhoim <cksgh201@naver.com>
2025-02-07 18:06:07 +0900

    QUARTZ-119 [Feat] 소프트 딜리트된 사용자의 로그인 불허

--- src/main/java/com/quartz/checkin/security/service/CustomUserDetailsService.java
+++ src/main/java/com/quartz/checkin/security/service/CustomUserDetailsService.java
@@ -31,6 +31,11 @@ public class CustomUserDetailsService implements UserDetailsService {
         Member member = memberRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 사용자입니다."));
 
+        if (member.getDeleted_at() != null) {
+            log.error("소프트 딜리트된 사용자가 로그인하려 합니다.");
+            throw new UsernameNotFoundException("소프트 딜리트된 사용자입니다.");
+        }
+
         return new CustomUser(
                 member.getId(),
                 member.getUsername(),

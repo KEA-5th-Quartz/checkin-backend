3ebe25f4ac3327a492ded2355d5908a7960e0036
chanhoim <cksgh201@naver.com>
2025-02-13 15:14:24 +0900

    QUARTZ-137 [Feat] Api 성공, 실패 응답 Matcher 구현

---
+++ src/test/java/com/quartz/checkin/util/ApiResponseMatchers.java
@@ -0,0 +1,31 @@
+package com.quartz.checkin.util;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Map;
+import org.hamcrest.Matcher;
+import org.springframework.test.web.servlet.ResultMatcher;
+public class ApiResponseMatchers {
+
+    public static ResultMatcher apiResponse(int expectedStatus,
+                                            Map<String, Matcher<?>> expectedDataFields) {
+        return result -> {
+            jsonPath("$.status", is(expectedStatus)).match(result);
+
+            if (expectedDataFields != null && !expectedDataFields.isEmpty()) {
+                expectedDataFields.forEach((key, matcher) -> {
+                    try {
+                        jsonPath("$.data." + key, matcher).match(result);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            } else {
+                jsonPath("$.data").value(anyOf(nullValue(), equalTo(""))).match(result);
+            }
+
+        };
+    }
+
+}

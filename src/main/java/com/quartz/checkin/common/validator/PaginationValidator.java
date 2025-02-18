3649fd96dbd25df8f93735d928f41878f26446ae
Kim CheolHwan <chelhwankim@gmail.com>
2025-02-18 15:50:06 +0900

    QUARTZ-163 [Fix] 페이지 수 및 사이즈 검증 순서 변경

--- src/main/java/com/quartz/checkin/common/validator/PaginationValidator.java
+++ src/main/java/com/quartz/checkin/common/validator/PaginationValidator.java
@@ -11,4 +11,15 @@ public class PaginationValidator {
             throw new ApiException(ErrorCode.INVALID_PAGE_NUMBER);
         }
     }
+
+    public static void validatePagination(int page, int totalPages) {
+        if (page > totalPages && totalPages > 0) {
+            throw new ApiException(ErrorCode.INVALID_PAGE_NUMBER);
+        }
+    }
+
+    public static void validateNumberAndSize(int page, int size) {
+        if (page < 1) throw new ApiException(ErrorCode.INVALID_PAGE_NUMBER);
+        if (size != 20 && size != 50 && size != 100) throw new ApiException(ErrorCode.INVALID_PAGE_SIZE);
+    }
 }
